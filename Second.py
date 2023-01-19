8757038848c2187aab6a54983af5ac476f886431
Akshat Chandra <akshatchandra2401@gmail.com>
2023-01-20 00:44:57 +0530

    CALCULATOR

---
+++ Second.py
@@ -0,0 +1,7 @@
+a=int(input());
+b=int(input());
+print("For", a,"+", b,"value is",a+b);
+print("For", a,"-", b,"value is",a-b);
+print("For", a,"*", b,"value is",a*b);
+print("For", a,"/", b,"value is",a/b);
+print("For", a,"%", b,"value is",a%b);
