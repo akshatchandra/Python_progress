26ab77fc7bb648e425ccd6d9e48502b0d4e75f38
Akshat Chandra <akshatchandra2401@gmail.com>
2023-02-07 00:34:28 +0530

    third day

---
+++ third.py
@@ -0,0 +1,17 @@
+##Variable
+num = 5
+print(id(num))
+name='abc'
+print(id(name))
+a=10
+b=a
+print(a)
+print(b)
+print(id(a))
+print(id(b))
+p=9
+print(type(p))
+
+## Data Types
+num =2.5
+print(type(num))
