5a236644593a6b8f22461e8f5c9fde6ec587085c
Akshat Chandra <akshatchandra2401@gmail.com>
2023-02-14 23:51:12 +0530

    12-18

---
+++ Number_System.py
@@ -0,0 +1,36 @@
+# print(0b0101)
+# print(oct(25))
+# print(hex(25))
+# print(0xa)
+
+#Swap two Variable
+a=3
+b=2
+print(a,b)
+# temp=a
+# a=b
+# b=temp
+# print(a,b)
+
+#Formula trick
+# a=a+b // 2+3=5 or a=a^b
+# b=a-b // 5-2=3 or b=a^b
+# a=a-b // 5-3=2 or a=a^b
+# print(a,b)
+
+#Bitwise Operator
+# print(~13) #Tilde Operator
+# print(24&13) #AND Operation
+# print(12|13) #OR Operation
+# print(10<<2) #Left-side shift
+# print(10>>2) #Right-side shift
+
+#import of math function
+import math
+x=math.sqrt(25)
+print(x)
+print(math.ceil(2.9))
+print(math.floor(2.9))
+print(math.pow(2,3))
+
+
