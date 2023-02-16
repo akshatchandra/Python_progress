ec48d0b582605b984646fc6f33993b70f3a67915
Akshat Chandra <akshatchandra2401@gmail.com>
2023-02-16 23:51:26 +0530

    pattern and for-else

---
+++ pattern.py
@@ -0,0 +1,39 @@
+# Square Patter
+# for i in range(0,4):
+#     for j in range(0,4):
+#         print("# ",end="")
+#     print()
+
+# Traingle Patter
+# for i in range(0,4):
+#     for j in range(i+1):
+#         print("# ",end="")
+#         j-=1
+#     print()
+
+# Reverse Triangle PAtter
+# for i in range(4):
+#     for j in range(4-i):
+#         print("# ", end="")
+#     print()
+
+################################
+# For Else
+################################
+
+# a=[12,13,14,1,2,29]
+# for i in a:
+#     if i % 5== 0:
+#         print(i)
+#         break
+# else:
+#     print("No number Found")
+
+# Prime Number
+# n=4
+# for i in range (2,n):
+#     if n%i==0:
+#         print("Not Prime")
+#         break
+# else:
+#     print("Prime Number")
