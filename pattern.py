a86f8324d046699750bddeb03805c330211065fc
Akshat Chandra <akshatchandra2401@gmail.com>
2023-02-20 23:48:54 +0530

    reverse pattern logic

--- pattern.py
+++ pattern.py
@@ -90,11 +90,19 @@
 #     print()
 
 # PYRAMID STAR PATTERN
+# n=int(input())
+# for i in range(1,n+1):
+#     for j in range(n-i):
+#         print(" ",end='')
+#     for k in range(2*i-1):
+#         print("*",end='')
+#     print()
+
+#REVERSE PYRAMID PATTERN
 n=int(input())
 for i in range(1,n+1):
-    for j in range(n-i):
-        print(" ",end='')
-    for k in range(2*i-1):
-        print("*",end='')
+    for j in range(i-1):
+        print(' ',end='')
+    for j in range(2*(n-i)+1):
+        print('*',end='')
     print()
-
