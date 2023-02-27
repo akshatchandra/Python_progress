8b1d3c64112c2105be88dc1822e69e73f18b6a2a
Akshat Chandra <akshatchandra2401@gmail.com>
2023-02-27 23:01:06 +0530

    day 2 of st's juts one code for continuity

--- function.py
+++ function.py
@@ -48,15 +48,22 @@
 # sum(1,3,2,1)
 
 
-def numpat(n):
-    num = 1
-    for i in range(0, n):
-        num = 1
-        for j in range(0, i + 1):
-            # printing number
-            print(num, end=" ")
-            num = num + 1
-        print("\r")
+# def num1(n):
+#     num = 1
+#     for i in range(0, n):
+#         num = 1
+#         for j in range(0, i + 1):
+#             # printing number
+#             print(num, end=" ")
+#             num = num + 1
+#         print("\r")
+# n = 5
+# num1(n)
 
-n = 5
-numpat(n)
+rows = int(input(" "))
+number = 1
+for i in range(1, rows+1):
+    for j in range(1, i+1):
+        print(number, end=" ")
+        number += 1
+    print()
