cca1bc435db76c343defb93f26bf4df627347c54
Akshat Chandra <akshatchandra2401@gmail.com>
2023-03-01 23:20:36 +0530

    st3

--- function.py
+++ function.py
@@ -60,10 +60,25 @@
 # n = 5
 # num1(n)
 
-rows = int(input(" "))
-number = 1
-for i in range(1, rows+1):
-    for j in range(1, i+1):
-        print(number, end=" ")
-        number += 1
+# rows = int(input(" "))
+# number = 1
+# for i in range(1, rows+1):
+#     for j in range(1, i+1):
+#         print(number, end=" ")
+#         number += 1
+#     print()
+
+# hourglass star pattern
+n = 5
+for i in range(n-1):
+    for j in range(i):
+        print(' ', end='')
+    for k in range(2*(n-i)-1):
+        print('*', end='')
+    print()
+for i in range(n):
+    for j in range(n-i-1):
+        print(' ', end='')
+    for k in range(2*i+1):
+        print('*', end='')
     print()
