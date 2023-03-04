22bfe4f255250ce409ada99757e3777493cde057
Akshat Chandra <akshatchandra2401@gmail.com>
2023-03-04 23:46:28 +0530

    fibonacii series and list in function

--- function and its argument.py
+++ function and its argument.py
@@ -32,17 +32,50 @@
 
 # Scope
 
-a =10
-def something():
-    a=14
-    # global a
+# a =10
+# def something():
+#     a=14
+#     # global a
+#
+#     # a=90
+#     x = globals()['a']
+#     # a=21
+#     print("in the function",a)
+#     globals()['a'] = 90
+# something()
+# print("outside the function",a)
 
-    # a=90
-    x = globals()['a']
-    # a=21
-    print("in the function",a)
-    globals()['a'] = 90
-something()
-print("outside the function",a)
+
+# List to a Function
+#
+# def count(list):
+#     even = 0
+#     odd = 0
+#
+#     for i in list:
+#         if i%2==0:
+#             even+=1
+#         else:
+#             odd+=1
+#     return even, odd
+#
+# list = [1,2,3,4,5,6,7,8,9,10]
+#
+# even, odd = count(list)
+# print(even)
+# print(odd)
 
 
+# Fibinaxcii Series
+def fibo(n):
+    a=0
+    b=1
+    print(a)
+    print(b)
+    for i in range(2,n):
+        # n-=1
+        c=a+b
+        a=b
+        b=c
+        print(c)
+fibo(5)
