8959ae9862312f8ef886cbd76324998137ccc29e
Akshat Chandra <akshatchandra2401@gmail.com>
2023-03-05 23:35:27 +0530

    Factorial

--- function and its argument.py
+++ function and its argument.py
@@ -67,15 +67,28 @@
 
 
 # Fibinaxcii Series
-def fibo(n):
-    a=0
-    b=1
-    print(a)
-    print(b)
-    for i in range(2,n):
-        # n-=1
-        c=a+b
-        a=b
-        b=c
-        print(c)
-fibo(5)
+# def fibo(n):
+#     a=0
+#     b=1
+#     print(a)
+#     print(b)
+#     for i in range(2,n):
+#         # n-=1
+#         c=a+b
+#         a=b
+#         b=c
+#         print(c)
+# fibo(5)
+
+# Factorial
+
+def fact(n):
+    f=1
+    for i in range(1,n+1):
+        f = f * i
+    return f
+x = int(input())
+# x=4
+
+result = fact(x)
+print(result)
