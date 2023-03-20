da75225de86aefb15490a14908fffc18edfb9123
Akshat Chandra <akshatchandra2401@gmail.com>
2023-03-20 23:56:25 +0530

    linear search

---
+++ Binary_search.py
@@ -0,0 +1,23 @@
+pos = -1
+def search(list ,n):
+    # i = 0
+
+    # while i < len(list):
+    #     if list[i] == n:
+    #         globals()['pos'] = i
+    #         return True
+    #     i = i + 1
+
+    for i in range(len(list)):
+        if list[i] == n:
+            print("Found")
+            globals()['pos'] = i
+            return True
+        i = i +1
+list = [5,8,4,6,9,2]
+
+n = 9
+if search(list,n):
+    print("Found at",pos+1)
+else:
+    print("Not Found")
