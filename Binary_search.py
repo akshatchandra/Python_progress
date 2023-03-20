c0ed1b91d76a102191c6ef79ae4dafd6648a0664
Akshat Chandra <akshatchandra2401@gmail.com>
2023-03-21 00:05:25 +0530

    binary search

--- Binary_search.py
+++ Binary_search.py
@@ -1,22 +1,49 @@
-pos = -1
-def search(list ,n):
-    # i = 0
+# pos = -1
+# def search(list ,n):
+#     # i = 0
+#
+#     # while i < len(list):
+#     #     if list[i] == n:
+#     #         globals()['pos'] = i
+#     #         return True
+#     #     i = i + 1
+#
+#     for i in range(len(list)):
+#         if list[i] == n:
+#             print("Found")
+#             globals()['pos'] = i
+#             return True
+#         i = i +1
+# list = [5,8,4,6,9,2]
 
-    # while i < len(list):
-    #     if list[i] == n:
-    #         globals()['pos'] = i
-    #         return True
-    #     i = i + 1
+# n = 9
+# if search(list,n):
+#     print("Found at",pos+1)
+# else:
+#     print("Not Found")
 
-    for i in range(len(list)):
-        if list[i] == n:
-            print("Found")
-            globals()['pos'] = i
+
+# Binary Search
+
+def search(list, n):
+    l = 0
+    u = len(list)-1
+
+    while l <= u:
+        mid  = (l+u) // 2
+
+        if list[mid]==n:
+            globals()['pos']=mid
             return True
-        i = i +1
-list = [5,8,4,6,9,2]
-
-n = 9
+        else:
+            if list[mid]<n:
+                l = mid+1;
+            else:
+                u = mid-1;
+    return False
+list = [4,7,8,12,45,99]
+# n = 4
+n=9
 if search(list,n):
     print("Found at",pos+1)
 else:
